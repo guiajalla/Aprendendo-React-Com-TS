8c1d513018b02f49399afac1fdb815038e272bfe
guiajalla <55240778+guiajalla@users.noreply.github.com>
2022-08-24 18:15:39 -0300

    useState adicionado

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,16 +3,28 @@ import Button from "../Button";
 import style from './Form.module.scss';
 
 class Form extends React.Component {
+    state = {
+        task: "",
+        time: "00:00"
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('state: ', this.state);
+    }
     render(){
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         Adicione um novo conteúdo:
                     </label>
                     <input 
                         type="text"
-                        name="task" id="task" 
+                        name="task" 
+                        value={this.state.task}
+                        onChange={event => this.setState({...this.state, task: event.target.value})}
+                        id="task" 
                         placeholder="O que você quer estudar?"
                         required
                     />
@@ -25,6 +37,8 @@ class Form extends React.Component {
                         type="time"
                         step="1"
                         name="time"
+                        value={this.state.time}
+                        onChange={event => this.setState({...this.state, time: event.target.value})}
                         id="time"
                         min="00:00:00"
                         max="01:30:00"
